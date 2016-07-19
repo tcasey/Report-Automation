0cca41900a2f8ef5640f120a84b5e714cd9c8776
tcasey <trevort.casey@gmail.com>
2016-07-19 14:53:21 -0600

    Split up reports. Working on logic for pagination

---
+++ acquisition.js
@@ -0,0 +1,175 @@
+var Horseman = require('node-horseman'),
+  pdfconcat = require('pdfconcat'),
+  request = require('request'),
+  util = require('util'),
+  fs = require('fs'),
+  baseURL = 'https://stag-cmo-la.convirza.com/#/',
+  filter = '&filter=America',
+  keys = require('./keys'),
+  date = [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  emailData = [],
+  obiOne,
+  routes = ['home'],
+  ou = 'yoda';
+
+//  horseman options can be added & set within this object
+var horseman = new Horseman({
+  switchToNewTab: true
+});
+
+////////////////////////
+/// Helper Functions ///
+////////////////////////
+
+//  Joins multiple single paged PDF's into a single multi-paged PDF. (Say that five times fast)
+function pdfUnite() {
+  return horseman.do(function(done) {
+    pdfconcat(['acquisition/' + emailData[0] +'-'+ routes[2] +'-'+ date[0] + '.pdf', 'acquisition/' + emailData[0]  +'-'+ routes[1] +'-'+ date[3] + '.pdf', 'acquisition/' + emailData[0] +'-'+ routes[0] +'-'+ date[6] + '.pdf'], 'horseman/Jedi.pdf', function(err) {
+      err ? console.log(err) : console.log('A new Jedi has been born');
+    });
+    setTimeout(done, 100);
+  })
+}
+
+function Jedi() {
+  return horseman.do(function(done) {
+    var obiOne = JSON.stringify(emailData[1]);
+    // console.log(obiOne);
+    setTimeout(done, 100);
+  })
+}
+
+function urlParse() {
+  return horseman.evaluate(function(ms, done) {
+    var url = document.URL;
+    done(null, url);
+  }, 500)
+
+  .then(function(data) {
+    var urlCurrent = data;
+
+    function cleanUrl(urlCurrent) {
+      return urlCurrent.split('?')[0];
+    }
+    var route = urlCurrent.substr(urlCurrent.indexOf("#") + 2);
+    console.log(cleanUrl(route));
+    routes.unshift(cleanUrl(route));
+  })
+}
+
+horseman
+  .userAgent('"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2790.0 Safari/537.36"')
+
+//  adds a cookie
+.cookies({
+    name: ou,
+    value: ou,
+    domain: '.convirza.com'
+  })
+  ////////////////////////
+  /////// Home ///////////
+  ////////////////////////
+  .viewport(780, 980)
+  //  opens & logs into cfa app
+  .open(baseURL + 'login/')
+  .type('input[id="email"]', keys.cfa_email)
+  .type('input[id="password"]', keys.cfa_pass)
+  .click('[id="b1"]')
+  .keyboardEvent('keypress', 16777221)
+
+.log('2 second delay')
+  .wait(2000)
+
+.url()
+  .log()
+
+.click('[id="leftmenu-trigger"]')
+
+.evaluate(function(ms, done) {
+  $('#page-heading').append('<span id=filter>yoda</span>');
+  $('img').hide();
+  var filter = $('#filter').text();
+  done(null, filter);
+}, 500)
+
+.then(function(data) {
+  emailData.push(data);
+})
+
+.wait(500)
+  .then(function() {
+    return horseman.pdf('acquisition/' + emailData[0]  +'-'+ routes[0] +'-'+ date[0] + '.pdf', {
+      format: 'A2',
+      orientation: 'portrait',
+      margin: '0.2in'
+    })
+  })
+
+.log('1 PDF')
+
+.then(function() {
+    return horseman.screenshot('acquisition/' + emailData[0] +'-'+ routes[0] +'-'+ date[1] + '.png')
+  })
+  .log('2 PNG')
+
+.then(function() {
+    return horseman.crop('.col-md-9', 'acquisition/' + emailData[0] +'-'+ routes[0] +'-'+ date[2] + '-cropped.png')
+  })
+  .log('3 PNG')
+
+/////////////////////////
+/// Reports Call Flow ///
+/////////////////////////
+
+.evaluate(function(ms, done) {
+  $('.fa-bar-chart-o').click();
+  $("span:contains('Acquisition')").click();
+  $("span:contains('Call Flows')").click();
+
+  done(null);
+}, 1000)
+
+.log('waiting.....')
+.wait(4000)
+
+.evaluate(function(ms, done) {
+  $('#page-heading').append('<span id=filter>yoda</span>');
+  $("button:contains('Reset Charts')").hide();
+  // $('#leftmenu-trigger').click();
+
+  done(null);
+}, 1000)
+
+.then(urlParse)
+
+.viewport(1080, 980)
+
+.then(function() {
+    return horseman.pdf('acquisition/' + emailData[0]  +'-'+ routes[0] +'-'+ date[6] + '.pdf', {
+      format: 'A2',
+      orientation: 'portrait',
+      margin: '0.2in'
+    })
+  })
+  .log('7 PDF')
+
+//   screenshot of view with appended
+.wait(1000)
+  .then(function() {
+    return horseman.screenshot('acquisition/' + emailData[0] +'-'+ routes[0] +'-'+ date[7] + '.png')
+  })
+  .log('8 PNG')
+
+//   captures cropped image of page view.
+.wait(1000)
+  .then(function() {
+    return horseman.crop('#callflowreport', 'acquisition/' + emailData[0] +'-'+ routes[0] +'-'+ date[8] + '-cropped.png')
+  })
+  .log('9 PNG')
+
+// .then(pdfUnite)
+
+.log(emailData)
+  .log(routes)
+
+.close();
--- callDetails.js
+++ callDetails.js
@@ -3,12 +3,13 @@ var Horseman = require('node-horseman'),
   request = require('request'),
   util = require('util'),
   fs = require('fs'),
-  baseURL = 'https://cfa.convirza.com/#/',
+  baseURL = 'https://stag-cmo-la.convirza.com/#/',
   filter = '&filter=America',
   keys = require('./keys'),
   date = [1, 2, 3, 4, 5, 6, 7, 8, 9],
   emailData = [],
   obiOne,
+  routes = ['home'],
   ou = 'yoda';
 
 //  horseman options can be added & set within this object
@@ -16,24 +17,65 @@ var horseman = new Horseman({
   switchToNewTab: true
 });
 
+////////////////////////
+/// Helper Functions ///
+////////////////////////
+
 //  Joins multiple single paged PDF's into a single multi-paged PDF. (Say that five times fast)
 function pdfUnite() {
   return horseman.do(function(done) {
-    pdfconcat(['horseman/' + emailData[0] + date[0] + '.pdf', 'horseman/' + emailData[0] + date[3] + '.pdf', , 'horseman/' + emailData[0] + date[6] + '.pdf'], 'horseman/Jedi.pdf', function(err) {
+    pdfconcat(['callDetails/' + emailData[0] +'-'+ routes[2] +'-'+ date[0] + '.pdf', 'callDetails/' + emailData[0]  +'-'+ routes[1] +'-'+ date[3] + '.pdf', 'callDetails/' + emailData[0] +'-'+ routes[0] +'-'+ date[6] + '.pdf'], 'callDetails/Jedi.pdf', function(err) {
       err ? console.log(err) : console.log('A new Jedi has been born');
     });
     setTimeout(done, 100);
   })
 }
 
-
 function Jedi() {
   return horseman.do(function(done) {
-  var obiOne = JSON.stringify(emailData[1]);
-  // console.log(obiOne);
+    var obiOne = JSON.stringify(emailData[1]);
+    // console.log(obiOne);
     setTimeout(done, 100);
   })
 }
+
+function urlParse() {
+  return horseman.evaluate(function(ms, done) {
+    var url = document.URL;
+    done(null, url);
+  }, 500)
+
+  .then(function(data) {
+    var urlCurrent = data;
+
+    function cleanUrl(urlCurrent) {
+      return urlCurrent.split('?')[0];
+    }
+    var route = urlCurrent.substr(urlCurrent.indexOf("#") + 2);
+    console.log(cleanUrl(route));
+    routes.unshift(cleanUrl(route));
+  })
+}
+
+// function paginationLogic() {
+//   return horseman.evaluate(function(ms, done) {
+//     var paginationIndicator = $('.mb10').text();
+//     done(null, paginationIndicator);
+//   }, 500)
+//
+//   .then(function(data) {
+//     var page = data;
+//
+//     function cleanUrl(page) {
+//       return page.split(' ').reverse();
+//     }
+//
+//     if (page[0] > page[2]) {
+//
+//     }
+//   })
+// }
+
 
 horseman
   .userAgent('"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2790.0 Safari/537.36"')
@@ -57,41 +99,43 @@ horseman
 
 .log('2 second delay')
   .wait(2000)
-  .url()
+
+.url()
   .log()
-  .click('[id="leftmenu-trigger"]')
+
+.click('[id="leftmenu-trigger"]')
 
 .evaluate(function(ms, done) {
   $('#page-heading').append('<span id=filter>yoda</span>');
   $('img').hide();
   var filter = $('#filter').text();
   done(null, filter);
-}, 1500)
+}, 500)
 
 .then(function(data) {
   emailData.push(data);
 })
 
-.wait(1000)
-.then(function() {
-  return horseman.pdf('horseman/' + emailData[0] + date[0] + '.pdf', {
-    format: 'A2',
-    orientation: 'portrait',
-    margin: '0.2in'
+.wait(500)
+  .then(function() {
+    return horseman.pdf('callDetails/' + emailData[0]  +'-'+ routes[0] +'-'+ date[0] + '.pdf', {
+      format: 'A2',
+      orientation: 'portrait',
+      margin: '0.2in'
+    })
   })
-})
 
-.log('yoda1 PDF')
-
-.then(function() {
-    return horseman.screenshot('horseman/' + emailData[0] + date[1] + '.png')
-  })
-  .log('yoda2 PNG')
+.log('1 PDF')
 
 .then(function() {
-    return horseman.crop('.col-md-9', 'horseman/' + emailData[0] + date[2] + 'cropped.png')
+    return horseman.screenshot('callDetails/' + emailData[0] +'-'+ routes[0] +'-'+ date[1] + '.png')
   })
-  .log('yoda3 PNG')
+  .log('2 PNG')
+
+.then(function() {
+    return horseman.crop('.col-md-9', 'callDetails/' + emailData[0] +'-'+ routes[0] +'-'+ date[2] + '-cropped.png')
+  })
+  .log('3 PNG')
 
 ////////////////////////
 //// Call Details //////
@@ -107,20 +151,21 @@ horseman
 }, 1000)
 
 .log('waiting.....')
-  .wait(4000)
-  .url()
-  .log()
-  .viewport(1100, 980)
+  .wait(2000)
+
+.then(urlParse)
+
+.viewport(1200, 980)
 
 .click('[id="leftmenu-trigger"]')
 
 .evaluate(function(ms, done) {
   $('#page-heading').append('<span id=filter>yoda</span>');
-    // $('.fa-calendar').click();
-    // $("li:contains('Last 30 Days')").click();
-    // setTimeout(10000);
-  $('.dropdown-toggle').click();
-  $("a:contains('CSV')").click();
+  // $('.fa-calendar').click();
+  // $("li:contains('Last 30 Days')").click();
+  // setTimeout(10000);
+  // $('.dropdown-toggle').click();
+  // $("a:contains('CSV')").click();
   $('img').hide();
   $('#simpleChart').hide();
   $('.fa-chevron-down').hide();
@@ -130,7 +175,7 @@ horseman
 
   var filter = $('#filter').text();
   done(null, filter);
-}, 1000)
+}, 500)
 
 //  this block is getting the clean ou bread crumb appended from above and then hiding it.
 //  only exists because current ou bred crumb has some weird stuff attatched to it.
@@ -138,87 +183,37 @@ horseman
   $('#page-heading').append('<span id=cleanBC>BreadCrumb</span>');
   var bc = $('#cleanBC').text();
   $('#cleanBC').hide();
-  $('#filter').hide();
 
   done(null, bc);
-}, 1000)
+}, 500)
 
 .then(function(data) {
-  emailData.push(data);
-})
-.then(Jedi)
+    emailData.push(data);
+  })
+  .then(Jedi)
 
 .then(function() {
-    return horseman.pdf('horseman/' + emailData[0] + date[3] + '.pdf', {
+    return horseman.pdf('callDetails/' + emailData[0]  +'-'+ routes[0] +'-'+ date[3] + '.pdf', {
       format: 'A2',
       orientation: 'portrait',
       margin: '0.2in'
     })
   })
-  .log('yoda4 PDF')
-
-//   screenshot of view with appended
-.then(function() {
-    return horseman.screenshot('horseman/' + emailData[0] + date[4] + '.png')
-  })
-  .log('yoda5 PNG')
-
-//   captures cropped image of page view.
-.then(function() {
-    return horseman.crop('.panel-inverse', 'horseman/' + emailData[0] + date[5] + 'cropped.png')
-  })
-  .log('yoda6 PNG')
-
-  /////////////////////////
-  /// Reports Call Flow ///
-  /////////////////////////
-
-.evaluate(function(ms, done) {
-  $('.fa-bar-chart-o').click();
-  $("span:contains('Acquisition')").click();
-  $("span:contains('Call Flows')").click();
-
-  done(null);
-}, 1000)
-
-.evaluate(function(ms, done) {
-  $('#page-heading').append('<span id=filter>yoda</span>');
-  $("button:contains('Reset Charts')").hide();
-  // $('#leftmenu-trigger').click();
-
-  done(null);
-}, 1000)
-
-.log('waiting.....')
-  .wait(4000)
-  .url()
-  .log()
-  .viewport(1080, 980)
-
+  .log('4 PDF')
 
 .then(function() {
-    return horseman.pdf('horseman/' + emailData[0] + date[6] + '.pdf', {
-      format: 'A2',
-      orientation: 'portrait',
-      margin: '0.2in'
-    })
+    return horseman.screenshot('callDetails/' + emailData[0] +'-'+ routes[0] +'-'+  date[4] + '.png')
   })
-  .log('yoda7 PDF')
+  .log('5 PNG')
 
-//   screenshot of view with appended
-.wait(1000)
 .then(function() {
-    return horseman.screenshot('horseman/' + emailData[0] + date[7] + '.png')
+    return horseman.crop('.panel-inverse', 'callDetails/' + emailData[0] +'-'+ routes[0] +'-'+ date[5] + '-cropped.png')
   })
-  .log('yoda8 PNG')
+  .log('6 PNG')
 
-//   captures cropped image of page view.
-.wait(1000)
-.then(function() {
-    return horseman.crop('#callflowreport', 'horseman/' + emailData[0] + date[8] + 'cropped.png')
-  })
-  .log('yoda9 PNG')
+  // .then(pdfUnite)
 
-.then(pdfUnite)
+  .log(emailData)
+    .log(routes)
 
 .close();
